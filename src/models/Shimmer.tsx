b39b848a02bd8305c2bd312ad17cfde1c2f3d102
Shaharyar Shamshi <shaharyarshamshi@gmail.com>
2024-02-22 20:24:25 +0530

    version upgrade

--- src/models/Shimmer.tsx
+++ src/models/Shimmer.tsx
@@ -13,6 +13,7 @@
 export interface ShimmerProps {
   width: number;
   height: number;
+  children: React.ReactNode;
   backgroundColor?: string;
   highlightColor?: string;
   animationDuration?: number;
